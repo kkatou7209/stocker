a2a13c5abaace1943cf46570c5ab598b62eb7942
kkatou7209 <kkatou7209@gmail.com>
2026-01-18 01:53:07 +0900

    refine number input

--- src/shared/ui/NumberInput.tsx
+++ src/shared/ui/NumberInput.tsx
@@ -10,17 +10,30 @@ const NumberInput: Component<{
 	const formatter = useFormat('ja-JP');
 
 	const onInput = (e: { currentTarget: HTMLInputElement }) => {
-		let value = e.currentTarget.value
-			.replace(/[^-\d.]+/g, '')
-			.replace(/^0+/, '');
 
-		value = value === '' ? '0' : value;
+		// Remove non-numeric and non-dot characters
+		let value = e.currentTarget.value.replace(/[^\d.]/g, '');
+
+		// Handle leading dot
+		if (value.startsWith('.')) {
+			value = `0${value}`;
+		}
+
+		// Allow only one dot
+		if (value.indexOf('.') !== value.lastIndexOf('.')) {
+
+			const parts = value.split('.');
+
+			// Keep only the first dot
+			value = `${parts[0]}.${parts[1]}`;
+		}
 
 		const num = Number(value);
 
+		// If not a number, reset to 0
 		if (Number.isNaN(num)) {
 			props.onInput?.(0);
-			e.currentTarget.value = value;
+			e.currentTarget.value = '0';
 			return;
 		}
 
@@ -29,23 +42,21 @@ const NumberInput: Component<{
 	};
 
 	const onChange = (e: { currentTarget: HTMLInputElement }) => {
-		let value = e.currentTarget.value
-            .replace(/[^-\d.]+/g, '')
-            .replace(/^0+/, '')
-            .replace(/^-+(?!\d+)/, '');
-
-		value = value === '' ? '0' : value;
+		
+		// Remove non-numeric and non-dot characters
+		const value = e.currentTarget.value.replace(/[^\d.]/g, '');
 
 		const num = Number(value);
 
+		// If not a number, reset to 0
 		if (Number.isNaN(num)) {
 			props.onChange?.(0);
-			e.currentTarget.value = value;
+			e.currentTarget.value = '0';
 			return;
 		}
 
 		props.onChange?.(num);
-		e.currentTarget.value = value;
+		e.currentTarget.value = num.toString();
 	};
 	return (
 		<label class="floating-label input">
